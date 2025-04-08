472251502a12a86a2b063d2222184492d9d9c5f7
R.Moroizumi <m.ryousuke0401@icloud.com>
2025-04-09 02:23:19 +0900

    ✨ スキーマ: 認証およびトークン関連のテーブルをエクスポートする新しいスキーマファイルを追加

---
+++ src/schemas/db/schema.ts
@@ -0,0 +1,12 @@
+// Auth tables
+export * from './table/auth/accounts';
+export * from './table/auth/authenticators';
+export * from './table/auth/sessions';
+export * from './table/auth/two_factor_confirmations';
+export * from './table/auth/users';
+export * from './table/auth/verification_tokens';
+
+// Token tables
+export * from './table/token/password_reset_tokens';
+export * from './table/token/registration_tokens';
+export * from './table/token/two_factor_tokens';
